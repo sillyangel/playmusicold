91984cfead0b1465ad9edeed2efffb453d0a9392
sillyangel <sillyangel3@gmail.com>
2023-10-11 00:26:58 +0000

    Fix update

--- sw.js
+++ sw.js
@@ -1,8 +1,32 @@
 self.addEventListener("fetch", event => {
-    // This is a dummy event listener
-    // just to pass the PWA installation criteria on
-    // some browsers
-  });
+   event.respondWith(
+       // Try to fetch the requested resource from the cache
+       caches.match(event.request)
+           .then(cachedResponse => {
+               // If the resource is found in the cache, return it
+               if (cachedResponse) {
+                   return cachedResponse;
+               }
+
+               // If the resource is not in the cache, fetch it from the network
+               return fetch(event.request)
+                   .then(response => {
+                       // Cache the fetched resource for future use
+                       caches.open("pwa-assets")
+                           .then(cache => {
+                               cache.put(event.request, response.clone());
+                           });
+                       return response;
+                   })
+                   .catch(error => {
+                       // Handle fetch errors, e.g., fallback to a custom offline page
+                       console.error('Fetch failed:', error);
+                       // You can return a custom offline page here
+                   });
+           })
+   );
+});
+
   
   const urlsToCache = ["sw.js", "main.css", "index.html", "app.webmanifest", "main.js", "firebas.js", "moblie.css", "moblie.html"];
   self.addEventListener("install", event => {
