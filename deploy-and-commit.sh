d6c708f0eb8c40cbc6da245c6ef3f40b1588859b
sillyangel <sillyangel3@gmail.com>
2023-10-11 01:24:50 +0000

    ahhhhhhhhhhhhhh

--- deploy-and-commit.sh
+++ deploy-and-commit.sh
@@ -1,15 +1,53 @@
 #!/bin/bash
 
-# Check if a commit message was provided as an argument
-if [ -z "$1" ]; then
-  echo "Please provide a commit message."
-  exit 1
+# This script simplifies the process of deploying code to Firebase with Git commits.
+# It also handles custom error messages and additional checks.
+
+# Prompt the user for a commit message
+echo "Please enter a commit message:"
+read commit_message
+
+# Check if the commit message is empty
+if [ -z "$commit_message" ]; then
+    echo "Error: commit message cannot be empty."
+    exit 1
 fi
 
-# Firebase deploy
-firebase deploy
+# Deploy to Firebase with a custom message
+echo "Deploying to Firebase with the following commit message:"
+echo ">> $commit_message"
+firebase deploy -m "$commit_message"
 
-# Git actions
+# Add all changes to the Git staging area
+echo "Adding changes to Git staging area..."
 git add .
-git commit -m "$1"
-git push
+
+# Commit changes with the provided message
+echo "Committing changes with message: $commit_message"
+git commit -m "$commit_message"
+
+# Push changes to the remote repository with a custom branch
+remote_branch="main"
+echo "Pushing changes to the remote repository to the '$remote_branch' branch..."
+git push origin "$remote_branch"
+
+# Check if the push was successful
+if [ $? -eq 0 ]; then
+    echo "Deployment and commit to '$remote_branch' branch successful!"
+else
+    echo "Error: failed to push changes to the '$remote_branch' branch."
+    exit 1
+fi
+
+# Check Firebase hosting status
+echo "Checking Firebase hosting status..."
+firebase hosting:channel:deploy "$commit_message"
+
+# If the deployment status is a success, confirm the deployment
+if [ $? -eq 0 ]; then
+    echo "Firebase hosting deployment is successful for commit message: $commit_message"
+else
+    echo "Error: Firebase hosting deployment failed for commit message: $commit_message"
+    exit 1
+fi
+
