d165466cd4f1e34e0696275933d97aa83c2c395a
sillyangel c <sillyangel3@gmail.com>
2023-10-18 20:39:45 +0000

    ahah

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   // The entry point file described above
-  entry: './src/firebase/firebase.mjs',
+  entry: './src/firebase/firebase.js',
   // The location of the build folder described above
   output: {
     path: path.resolve(__dirname, './src/dist'),
